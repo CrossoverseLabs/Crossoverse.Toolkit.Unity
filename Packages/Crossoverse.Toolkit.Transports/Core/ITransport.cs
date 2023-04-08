ab17f50ea60c6f7c7bca0c2e73d4c22b1a3af5e2
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-04-08 21:42:02 +0900

    Add Crossoverse.Toolkit.Transports

---
+++ Packages/Crossoverse.Toolkit.Transports/Core/ITransport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Crossoverse.Toolkit.Transports
+{
+    public interface ITransport : IDisposable
+    {
+        event Action OnConnected;
+        event Action OnDisconnected;
+        event Action<byte[]> OnReceiveMessage;
+        
+        bool IsConnected { get; }
+        int ClientId { get; }
+        
+        Task<bool> ConnectAsync(string roomId = "");
+        Task DisconnectAsync();
+        
+        void Send(ArraySegment<byte> data);
+    }
+}
