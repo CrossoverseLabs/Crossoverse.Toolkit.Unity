1e89633a44ccd3f6fe61affafe66b822a8c67aa3
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-08-05 22:54:14 +0900

    feat: Add BufferingKey

---
+++ Packages/Crossoverse.Toolkit.Transports/Core/SendOptions/BufferingKey.cs
@@ -0,0 +1,9 @@
+namespace Crossoverse.Toolkit.Transports
+{
+    public struct BufferingKey
+    {
+        public object FirstKey;
+        public object SecondKey;
+        public object ThirdKey;
+    }
+}
